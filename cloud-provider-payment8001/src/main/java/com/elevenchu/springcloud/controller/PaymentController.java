3ca4dcdcb383c20000e5371c4c531e1ac1c8e37a
elevenChu <964737042@qq.com>
2021-09-20 17:14:04 +0800

    9.20-支付模块/yml修改

---
+++ cloud-provider-payment8001/src/main/java/com/elevenchu/springcloud/controller/PaymentController.java
@@ -0,0 +1,47 @@
+package com.elevenchu.springcloud.controller;
+
+import com.elevenchu.springcloud.entities.CommonResult;
+import com.elevenchu.springcloud.entities.Payment;
+import com.elevenchu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController {
+        @Resource
+        private PaymentService paymentService;
+        @PostMapping("/payment/create")
+        public CommonResult create(Payment payment){
+            int result = paymentService.create(payment);
+            log.info("********插入结果"+result);
+            if (result>0){
+                return  new CommonResult(200,"插入数据库成功",result);
+            }else {
+                return new CommonResult(444,"插入数据库失败",null);
+            }
+        }
+
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id)
+    {
+        Payment payment = paymentService.getPaymentById(id);
+
+        if(payment != null)
+        {
+            return new CommonResult(200,"查询成功 ",payment);
+        }else{
+            return new CommonResult(444,"没有对应记录,查询ID: "+id,null);
+        }
+    }
+
+
+
+
+}
---
+++ cloud-provider-payment8001/src/main/java/com/elevenchu/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,28 @@
+package com.elevenchu.springcloud.service.impl;
+
+import com.elevenchu.springcloud.dao.PaymentDao;
+import com.elevenchu.springcloud.entities.Payment;
+import com.elevenchu.springcloud.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+
+
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
