bee52509936765afdec36dc908e79e8e72f065e5
elevenChu <964737042@qq.com>
2021-09-22 19:53:41 +0800

    9.22-instance实例的名字与DiscoveryClient的实现

--- cloud-provider-payment8001/src/main/java/com/elevenchu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/elevenchu/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.elevenchu.springcloud.entities.Payment;
 import com.elevenchu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -17,6 +20,10 @@ public class PaymentController {
 
         @Value("${server.port}")
         private String serverPort;
+
+        @Resource
+        private DiscoveryClient discoveryClient;
+
         @PostMapping("/payment/create")
         public CommonResult create(@RequestBody Payment payment){
             int result = paymentService.create(payment);
@@ -43,6 +50,21 @@ public class PaymentController {
     }
 
 
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery()
+    {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("*****element: "+element);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+
+        return this.discoveryClient;
+    }
 
 
 }
