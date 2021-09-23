68451488ec92c68d8fb10a920a8b791d22258c48
elevenChu <964737042@qq.com>
2021-09-23 16:54:31 +0800

    9.23-Ribbon负载规则替换

--- cloud-consumer-order80/src/main/java/com/elevenchu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/elevenchu/springcloud/OrderMain80.java
@@ -1,11 +1,14 @@
 package com.elevenchu.springcloud;
 
+import com.elevenchu.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(value = "CLOUD_PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class OrderMain80
 {
     public static void main( String[] args ){
