d98dd2987f497be000e5eca032de8efdc1058b1c
elevenChu <964737042@qq.com>
2021-09-23 16:46:00 +0800

    9.23-RestTemplate的调用

--- cloud-consumer-order80/src/main/java/com/elevenchu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/elevenchu/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.elevenchu.springcloud.controller;
 import com.elevenchu.springcloud.entities.CommonResult;
 import com.elevenchu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +29,14 @@ public class OrderController {
       return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
   }
 
-
+  @GetMapping("/consumer/payment/getForEntity/{id}")
+  public CommonResult<Payment> getPayment2(@PathVariable("id") Long id){
+    ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    if(entity.getStatusCode().is2xxSuccessful()){
+      return entity.getBody();
+    }else{
+      return new CommonResult<>(444,"操作失败");
+    }
+  }
 
 }
