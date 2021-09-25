d9beac2b11201a8f3c0deed92cb3ca725bb08d8d
elevenChu <964737042@qq.com>
2021-09-25 19:32:40 +0800

    9.25-Hystrix卡顿

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/elevenchu/springcloud/service/PaymentHystrixService.java
@@ -0,0 +1,17 @@
+package com.elevenchu.springcloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient("CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+public interface PaymentHystrixService {
+    @GetMapping("/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id);
+
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id);
+
+}
