3db47eab2a7d93db7ddf8a20a5b0b4554cf63f26
elevenChu <964737042@qq.com>
2021-10-08 16:37:08 +0800

    10.8-nacos服务提供者注册1

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/elevenchu/springcloud/controller/PaymentController.java
@@ -0,0 +1,16 @@
+package com.elevenchu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+    @Value("serverPort")
+    private String serverPort;
+    @GetMapping("/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id){
+        return  "nacos registry, serverPort: "+ serverPort+"\t id"+id;
+    }
+}
