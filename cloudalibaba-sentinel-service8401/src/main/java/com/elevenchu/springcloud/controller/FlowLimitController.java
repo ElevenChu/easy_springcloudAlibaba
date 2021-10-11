a376d1863d03c0a65bc5558e1d433b8d46251414
elevenChu <964737042@qq.com>
2021-10-11 21:25:11 +0800

    10.11-sentinel熔断降级RT->平均响应时间

--- cloudalibaba-sentinel-service8401/src/main/java/com/elevenchu/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/elevenchu/springcloud/controller/FlowLimitController.java
@@ -4,23 +4,35 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 @Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
-    public String testA()
-    {
+    public String testA() {
         return "------testA";
     }
 
     @GetMapping("/testB")
-    public String testB()
-    {
-        log.info(Thread.currentThread().getName()+"\t"+"...testB");
+    public String testB() {
+        log.info(Thread.currentThread().getName() + "\t" + "...testB");
         return "------testB";
     }
 
+    @GetMapping("/testD")
+    public String testD() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        log.info("testD 测试RT");
+        return "==========testD";
+    }
 
 
 
