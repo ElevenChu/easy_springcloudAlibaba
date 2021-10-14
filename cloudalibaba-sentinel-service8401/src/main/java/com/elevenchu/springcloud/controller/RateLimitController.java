e5a6e4f377bdd675a43862f41f9771ecbc8df1ab
elevenChu <964737042@qq.com>
2021-10-14 15:27:51 +0800

    10.14-sentinelResource配置2

--- cloudalibaba-sentinel-service8401/src/main/java/com/elevenchu/springcloud/controller/RateLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/elevenchu/springcloud/controller/RateLimitController.java
@@ -4,6 +4,7 @@ import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.elevenchu.springcloud.entities.CommonResult;
 import com.elevenchu.springcloud.entities.Payment;
+import com.elevenchu.springcloud.myhandler.CustomerBlockHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,25 @@ public class RateLimitController {
     public CommonResult handleException(BlockException exception) {
         return new CommonResult(444,exception.getClass().getCanonicalName()+"\t 服务不可用");
     }
+    @GetMapping("/rateLimit/byUrl")
+    @SentinelResource(value = "byUrl")
+    public CommonResult byUrl()
+    {
+        return new CommonResult(200,"按url限流测试OK",new Payment(2020L,"serial002"));
+    }
+
+    @GetMapping("/rateLimit/customerBlockHandler")
+    @SentinelResource(value = "customerBlockHandler",
+            blockHandlerClass = CustomerBlockHandler.class,//<-------- 自定义限流处理类
+            blockHandler = "handlerException2")//<-----------
+    public CommonResult customerBlockHandler()
+    {
+        return new CommonResult(200,"按客戶自定义",new Payment(2020L,"serial003"));
+    }
+
+
+
+
 }
 
 
